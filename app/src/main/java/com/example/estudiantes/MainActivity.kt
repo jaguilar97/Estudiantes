7e53357a36b2486641ca43187d2f2c5f4d083c65
Johan <aguilar.garita97@gmail.com>
2022-02-25 09:15:37 -0600

    Tarea 02 login

--- app/src/main/java/com/example/estudiantes/MainActivity.kt
+++ app/src/main/java/com/example/estudiantes/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.estudiantes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.estudiantes.databinding.ActivityMainBinding
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
@@ -44,6 +46,30 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun ingresa() {
-        TODO("Not yet implemented")
+        val email = binding.txtCorreo.text.toString()
+        val clave = binding.txtContraseA.text.toString()
+
+        auth.signInWithEmailAndPassword(email,clave).addOnCompleteListener(this){ task ->
+            if (task.isSuccessful){
+                val user = auth.currentUser
+                actualiza(user)
+            } else {
+                Toast.makeText(baseContext,getString(R.string.msg_fallo_login), Toast.LENGTH_SHORT).show()
+                actualiza(null)
+            }
+        }
+    }
+
+    private fun actualiza(user: FirebaseUser?) {
+        if(user!=null){
+            val intent = Intent(this,Principal::class.java)
+            startActivity(intent)
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        val user = auth.currentUser
+        actualiza(user)
     }
 }
