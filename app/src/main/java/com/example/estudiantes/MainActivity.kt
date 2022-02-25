0b1790e3b20fca207a4c455a4eabedc77788bb09
Johan <aguilar.garita97@gmail.com>
2022-02-24 18:36:47 -0600

    Tarea 02

--- app/src/main/java/com/example/estudiantes/MainActivity.kt
+++ app/src/main/java/com/example/estudiantes/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.estudiantes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.estudiantes.databinding.ActivityMainBinding
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        FirebaseApp.initializeApp(this)
+        auth = Firebase.auth
+
+        binding.btnIngresar.setOnClickListener { ingresa() }
+        binding.btnRegistrar.setOnClickListener { registrarse() }
+
+    }
+
+    private fun registrarse() {
+        val email = binding.txtCorreo.text.toString()
+        val clave = binding.txtContraseA.text.toString()
+
+        auth.createUserWithEmailAndPassword(email,clave).addOnCompleteListener(this){ task ->
+            if (task.isSuccessful){
+                val user = auth.currentUser
+                actualiza(user)
+            } else {
+                Toast.makeText(baseContext,getString(R.string.msg_fallo_registro), Toast.LENGTH_SHORT).show()
+                actualiza(null)
+            }
+        }
+
+    }
+
+    private fun ingresa() {
+        TODO("Not yet implemented")
     }
 }
